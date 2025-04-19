4d00192760bba24df56435c02bc24ba6f60b6dba
Luke Zhuang <lukezhuang@Lukes-Laptop-7.local>
2025-04-18 17:23:37 -0700

    set up my projected

---
+++ my-app/src/HomePage.js
@@ -0,0 +1,15 @@
+import './HomePage.css';
+
+function HomePage() {
+  return (
+    <div className="HomePage">
+      <header className="HomeHeader">
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default HomePage;

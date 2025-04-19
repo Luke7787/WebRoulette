828e328c85a2868f96c6dcd8a28b34fd2020bde7
Luke Zhuang <lukezhuang@Lukes-Laptop-7.lan>
2025-04-19 13:19:19 -0700

    updated the description

--- my-app/src/HomePage.js
+++ my-app/src/HomePage.js
@@ -7,7 +7,12 @@ function HomePage() {
         <h1 className="SiteTitle">WebRoulette</h1>
       </header>
       <p className="Description">
-      Welcome to WebRoulette! With just a click, <br /> you’ll be taken to a random website on the internet!
+      Welcome to WebRoulette! With just a click, <br /> 
+      you’ll be taken to a random website on the internet! <br /> <br />
+
+      Use Guest mode to explore random websites without signing in. <br />
+      Login to access features like saving your favorite websites <br />
+      or participating in forum discussions.
       </p>
       <div className="ButtonGroup">
         <button className="Button Login">Login</button>

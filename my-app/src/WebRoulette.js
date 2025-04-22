54f3e1aa8616b76ec1d87ccde289e5293a42ecb6
Luke Zhuang <lukezhuang@Lukes-Laptop-7.lan>
2025-04-22 16:01:40 -0700

    update

--- my-app/src/WebRoulette.js
+++ my-app/src/WebRoulette.js
@@ -1,14 +1,18 @@
 import React from 'react';
+import './WebRoulette.css';
 
 function WebRoulette({ isLoggedIn }) {
   return (
-    <div>
-      <h2>{isLoggedIn ? "Welcome, User!" : "Welcome, Guest!"}</h2>
-      <p>
+    <div className = "WebRouletteContainer">
+      <h2 className = "WelcomeHeader">
+        {isLoggedIn ? "Welcome, User!" : "Welcome, Guest!"}
+      </h2>
+      <p className = "WelcomeDescription">
         {isLoggedIn
           ? "You can now save your favorite websites and join discussions."
           : "You are exploring random websites as a guest."}
       </p>
+      <button className="RandomButton">Random??</button>
     </div>
   );
 }
